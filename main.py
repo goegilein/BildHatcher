591070e9944f4d0109b2a998c031a1507d149b47
goegi_kleiner <jonas.jz.zipfel@gmail.com>
2025-06-25 20:18:48 +0200

    another test

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ if __name__ == "__main__":
     gui.image_item = QtWidgets.QGraphicsPixmapItem()
     gui.image_scene.addItem(gui.image_item)
     
+    
     gui.show()
         
     data_handler= DataHandling.DataHandler(gui)
